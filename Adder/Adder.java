51e1e41ae4b6f2b65ec400aabbff7fa7872b24fc
tHei3est <5710110481@psu.ac.th>
2019-03-01 16:35:38 +0700

    Update

---
+++ Adder/Adder.java
@@ -0,0 +1,27 @@
+public class Adder {
+    private double val;
+
+    public Adder() {
+        val = 0.0;
+    }
+
+    public void add(double v) {
+        val += v;
+    }
+
+    public void addFrom(Adder another) {
+        val += another.getVal();
+    }
+
+    public void reset() {
+        val = 0;
+    }
+
+    public void setVal(double v) {
+        val = v;
+    }
+
+    public double getVal() {
+        return val;
+    }
+}
---
+++ Adder/AdderTest.java
@@ -0,0 +1,17 @@
+public class AdderTest {
+
+    public static void main (String args[]) {
+        Adder a = new Adder();
+        a.add(2);
+        a.add(3);
+        System.out.println(a.getVal());
+
+        Adder b = new Adder();
+        b.setVal(12);
+        b.add(1);
+        System.out.println(b.getVal());
+
+        a.addFrom(b);
+        System.out.println(a.getVal());
+    }
+}
